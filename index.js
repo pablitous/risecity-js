f70671de3869a3a8c8ec8ec551e59d36218fe0a7
Pablo Santamaria <mapasa250@hotmail.com>
2021-12-01 16:14:34 -0300

    fixed

--- index.js
+++ index.js
@@ -14,7 +14,8 @@
             
             var els = document.getElementsByClassName('btn btn-secondary itemmap houseinteract');
 
-            Array.prototype.forEach.call(els, async function(el) {
+            //Array.prototype.forEach.call(els, async function(el) {
+                for(const el of els){
                 // Do stuff here
                 if (el.getAttribute('aria-label').includes('Building') && !el.getAttribute('class').includes('opacity')) {
                     console.log('Lo tengo comprado');
@@ -24,26 +25,41 @@
                     await sleep(getRandomInt(2000, 4000));
                     percent = parseFloat(document.getElementsByClassName('featured_modal')[0].getElementsByClassName('progress-bar bg-info')[0].getAttribute('aria-valuenow'));
                     if (percent > 30) {
-                        //document.getElementsByClassName('featured_modal')[0].getElementsByClassName('btn btn-primary')[0].click();
+                        document.getElementsByClassName('awesome claimcoins')[0].click();
                         console.log('aca recojo las monedas');
                     }else{
                         console.log('aca no recojo');
                     }
                     await sleep(getRandomInt(2000, 4000));
+                    //close panel
                     document.getElementsByClassName('awesome scrap closehousepanel')[0].click();
                 }
-            });
+            };
 
 
             await sleep(getRandomInt(3000, 6000));
          
-            var timerMainLogic = getRandomInt(300000, 1080000);
+            var timerMainLogic = getRandomInt(1480000, 4080000);
             timerMainLogicNum = timerMainLogic / 1000 / 60;
             console.log(
                 dateNow()+" Waiting " + timerMainLogicNum.toFixed(2) + " minutes for next check"
             );
             await sleep(timerMainLogic);
         }
+    }
+    
+    async function getDailyReward() {
+        
+        if (document.querySelectorAll('[aria-label="Experience Box"]').length > 0) {
+            document.querySelectorAll('[aria-label="Experience Box"]')[0].click();
+            await sleep(getRandomInt(3000, 6000));
+            document.getElementsByClassName('awesome claimres')[0].click();
+            //document.getElementsByClassName('awesome scrap closehousepanel')[0].click();
+            console.log("Reward has been claimed");
+        }else{
+            console.log("Reward already claimed");
+        }
+       /* <img src="/styles/img/casas/daily.png" id="PhA22r" class="btn btn-secondary itemmap materialinteract infocasas" data-bs-toggle="tooltip" data-bs-placement="top" style="top:846px;left:1931px" title="" aria-label="Experience Box" data-bs-original-title="Experience Box"></img>*/
     }
     
     function dateNow() {
@@ -71,4 +87,5 @@
         );
     };
 
-    mainLogic();
+    //mainLogic();
+    getDailyReward();
